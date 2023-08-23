bac431d62eb51771c84a2b49bfbffee69da8aef6
ashjeong <ashjeong@umich.edu>
2023-08-23 16:43:31 -0400

    FIX: how to css styling to new file
    css inline styling in how to changed to a file
    added dividers

--- src/extras/how_to.js
+++ src/extras/how_to.js
@@ -1,5 +1,6 @@
 import { Button, Modal } from 'react-bootstrap';
 import { useState } from 'react';
+import './how_to.css'
 
 export function HowtoModal (props) {
     return (
@@ -8,6 +9,7 @@ export function HowtoModal (props) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
@@ -15,49 +17,51 @@ export function HowtoModal (props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p style={{fontSize:"20px"}}>I've picked a 4-digit number. It's up to you to guess it! </p>
-        <h4>Valid numbers</h4>
-        <p style={{fontSize:"20px"}}>A valid guess or answer has 4 unique digits of 0-9.</p>
+        <p style={{fontSize:"20px"}}><strong>Guess the 4-digit number!</strong></p>
+        <hr className="mb-3 mt-4" style={{width:"80%", margin:"auto"}}/>
+        <h4>Valid Numbers</h4>
+        <p style={{fontSize:"20px"}}>A valid guess or answer has 4 <font style={{textDecoration:"underline"}}>unique</font> digits of 0-9.</p>
         <center>
-          <div style={{fontSize:"20px", marginBottom:"15px"}}>For example:</div>
-          <div style={{display:"flex", width:"100%", justifyContent:"space-around"}}>
-            <div style={{display:"block", fontSize:"40px"}}>
-              <font style={{backgroundColor:"lightgreen"}}>0382</font>
-              <div>valid</div>
+          <div style={{fontSize:"20px", marginBottom:"10px"}}>For example:</div>
+          <div className="HowTo-example">
+            <div className="HowTo-example-block">
+              <font className="HowTo-example-correct">0382</font>
+              <div style={{fontSize:"25px"}}>valid</div>
             </div>
-            <div style={{display:"block", fontSize:"40px"}}>
-                <font style={{backgroundColor:"lightpink"}}>3</font>0<font style={{backgroundColor:"lightpink"}}>3</font>4
-                <div>invalid</div>
+            <div className="HowTo-example-block">
+                <font className="HowTo-example-wrong">3</font>0<font className="HowTo-example-wrong">3</font>4
+                <div style={{fontSize:"25px"}}>invalid</div>
             </div>
           </div>
           <br></br>
         </center>
+        <hr className="mb-4" style={{width:"80%", margin:"auto"}}/>
         <h4>Guessing Feedback</h4>
         <p style={{fontSize:"20px"}}>
-          After submitting a number, I'll give you some feedback to tell you how close you are to my number!<br></br>
+          After guessing a number, I'll give you some feedback to tell you how close you are to my number!<br></br>
         </p>
         <font style={{fontSize:"18px"}}><center><strong>To win, you need 4 Strikes!<br></br></strong></center></font>
-          <strong>"S": Strikes</strong> - You have a digit that is the correct number and in the same position as in the answer. <br></br>
-          <strong>"B": Balls</strong> - You have a digit that is included in the answer, but it isn't in the correct position. <br></br><br></br>
+          <strong>Strikes (S)</strong> - You have a digit that is the correct number and in the same position as in the answer. <br></br>
+          <strong>Balls (B)</strong> - You have a digit that is included in the answer, but it isn't in the correct position. <br></br><br></br>
         <center>
           <div style={{fontSize:"20px"}}>
             Example: 1 Strike, 1 Ball ("
-            <font style={{backgroundColor:"lightgreen"}}>1S</font> &nbsp;
-            <font style={{backgroundColor:"lightskyblue", textDecoration:"underline"}}>1B</font>"
+            <font className="HowTo-example-correct">1S</font> &nbsp;
+            <font className="HowTo-example-ball">1B</font>"
             )
             </div>
-          <div style={{display:"flex", width:"100%", justifyContent:"space-around"}}>
-            <div style={{display:"block", fontSize:"40px"}}>
+          <div className="HowTo-example">
+            <div className="HowTo-example-block">
               <div>Answer</div>
               30
-              <font style={{backgroundColor:"lightskyblue", textDecoration:"underline"}}>2</font>
-              <font style={{backgroundColor:"lightgreen"}}>9</font>
+              <font className="HowTo-example-ball">2</font>
+              <font className="HowTo-example-correct">9</font>
             </div>
-            <div style={{display:"block", fontSize:"40px"}}>
+            <div className="HowTo-example-block">
               <div>Guess</div>
-              <font style={{backgroundColor:"lightskyblue", textDecoration:"underline"}}>2</font>
+              <font className="HowTo-example-ball">2</font>
               87
-              <font style={{backgroundColor:"lightgreen"}}>9</font>
+              <font className="HowTo-example-correct">9</font>
             </div>
           </div>
         </center>
@@ -73,8 +77,8 @@ function HowTo() {
     const [modalShow, setModalShow] = useState(false);
     return (
     <>
-        <Button style={{padding:"5px 10px 5px 10px"}} onClick={() => setModalShow(true)}>
-            How to Play~
+        <Button className="HowTo-button" style={{padding:"5px 10px 5px 10px"}} onClick={() => setModalShow(true)}>
+            How to Play
         </Button>
         <HowtoModal
             show={modalShow}
